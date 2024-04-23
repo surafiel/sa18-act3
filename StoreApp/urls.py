6c00c806d59a806f95d20ef775d94c6bad853080
Surafiel Negatu <surafielnegatu@gmail.com>
2024-04-23 01:57:15 -0500

    third commit

--- StoreApp/urls.py
+++ StoreApp/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from myapp import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('products/', views.index, name='index'),
+    path('products/<int:id>/', views.show, name='show'),
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import Product
 
-# Create your views here.
+def index(request):
+    products = Product.objects.all()
+    return render(request, 'myapp/index.html', {'products': products})
+
+def show(request, id):
+    product = Product.objects.get(pk=id)
+    return render(request, 'myapp/show.html', {'product': product})
